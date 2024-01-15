886f2eaf5a2f0a6785286095285421a0ad10dc82
Semih <semih.turkoglu@student.vinci.be>
2024-01-15 17:32:15 +0100

    add feature: gamertag must be at least 8 characters

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -7,11 +7,6 @@ describe('validations tests suites - isValid', () => {
         expect(result).toBe(false);
     });
 
-    test('should return true or false as the function is defined', () => {
-        const result = isValid('gamertag');
-        expect(result).toBe(true);
-    });
-
     test('should return "Invalid - gamertag length must be at least 8 characters"', () => {
         const result = isValid('short');
         expect(result).toBe(
@@ -19,5 +14,21 @@ describe('validations tests suites - isValid', () => {
         );
     });
 
-    
+    test('should return “Invalid - gamertag must contain at least a special character”', () => {
+        const result = isValid('gamertag');
+        expect(result).toBe(
+            'Invalid - gamertag must contain at least a special character'
+        );
+    });
+
+    test('should return “Invalid - gamertag must contain at least a number”', () => {
+        const result = isValid('gamertag$');
+        expect(result).toBe(
+            'Invalid - gamertag must contain at least a special character'
+        );
+    });
+    test('should return true if the gamertag is valid', () => {
+        const result = isValid('gamertag&1');
+        expect(result).toBe(true);
+    });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -6,7 +6,36 @@ const isValid = (gamertag) => {
     if (gamertag.length < 8) {
         return 'Invalid - gamertag length must be at least 8 characters';
     }
-    return true;
+    if (
+        !(
+            gamertag.includes('&') ||
+            gamertag.includes('$') ||
+            gamertag.includes('!') ||
+            gamertag.includes('è') ||
+            gamertag.includes('§') ||
+            gamertag.includes('à')
+        )
+    ) {
+        return 'Invalid - gamertag must contain at least a special character';
+    }
+    if (
+        !(
+            gamertag.includes('1') ||
+            gamertag.includes('2') ||
+            gamertag.includes('3') ||
+            gamertag.includes('4') ||
+            gamertag.includes('5') ||
+            gamertag.includes('6') ||
+            gamertag.includes('7') ||
+            gamertag.includes('8') ||
+            gamertag.includes('9') ||
+            gamertag.includes('0')
+        )
+    ) {
+        return 'Invalid - gamertag must contain at least a number';
+    } else {
+        return true;
+    }
 };
 
 exports.isValid = isValid;
